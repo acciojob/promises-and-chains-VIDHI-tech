5f5d6a1d536b4700a2e2f4a86075db50614c854e
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-24 13:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
+
+      const ageInput = document.getElementById("age");
+      const nameInput = document.getElementById("name");
+
+      if (ageInput.value.trim() === "" || nameInput.value.trim() === "") {
+        alert("Please fill in all fields.");
+        return;
+      }
+
+      const promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          const age = parseInt(ageInput.value);
+          const name = nameInput.value;
+
+          if (age >= 18) {
+            resolve(name);
+          } else {
+            reject(name);
+          }
+        }, 4000);
+      });
+
+      promise
+        .then(function(name) {
+          alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(function(name) {
+          alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
+
+      ageInput.value = "";
+      nameInput.value = "";
+    });
