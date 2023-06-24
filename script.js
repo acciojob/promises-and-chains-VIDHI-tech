e89841e45e8d682a8969419a27190280f18c7e6a
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-24 13:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-      event.preventDefault(); // Prevent form submission
-
-      const ageInput = document.getElementById("age");
-      const nameInput = document.getElementById("name");
-
-      if (ageInput.value.trim() === "" || nameInput.value.trim() === "") {
-        alert("Please fill in all fields.");
-        return;
-      }
-
-      const promise = new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          const age = parseInt(ageInput.value);
-          const name = nameInput.value;
-
-          if (age >= 18) {
-            resolve(name);
-          } else {
-            reject(new Error(`Oh sorry ${name}. You aren't old enough.`));
-          }
-        }, 4000);
-      });
-
-      promise
-        .then(function(name) {
-          alert(`Welcome, ${name}. You can vote.`);
-        })
-        .catch(function(error) {
-          alert(error.message);
-        });
-
-      ageInput.value = "";
-      nameInput.value = "";
+function submitForm(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  const ageInput = document.getElementById('age');
+  const nameInput = document.getElementById('name');
+  
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+  
+  if (age && name) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve(name);
+        } else {
+          reject(name);
+        }
+      }, 4000);
     });
+    
+    promise
+      .then((name) => {
+        alert(`Welcome, ${name}. You can vote.`);
+      })
+      .catch((name) => {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+      });
+      
+    ageInput.value = '';
+    nameInput.value = '';
+  }
+}
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', submitForm);
